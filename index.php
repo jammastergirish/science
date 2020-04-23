4dbee3598620e1ad8edba6a8f3bfc740a1523fed
Girish Gupta <girish@girish-gupta.com>
2020-04-23 07:06:14 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>Science</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<h1>Science</h1>
+<ul>
+<li><a href="20040323_FlashRateMonitor.pdf">Flash-rate monitor</a></li>
+<li><a href="20050323_SpecialRelativity.pdf">Special Relativity</a></li>
+<lI><a href="20070922_PhysicsOfMusic.pdf">Physics of Music</a></li>
+<li><a href="20081100_NegativeIndexOfRefraction.pdf">Negative Index of Refraction</a></li>
+<li>Role of the Pulmonary Vein in the Genesis of Atrial Fibrillation: Insights from a Biophysically Detailed Computational Model
+    <ul>
+        <li><a href="https://github.com/jammastergirish/MPhysAtrialFibrillation/blob/master/Final%20Report/FINALREPORT.pdf">Final Report</a></li>
+        <li><a href="https://github.com/jammastergirish/MPhysAtrialFibrillation/blob/master/Aslanidi_etal_Manchester_2010.pdf">Poster for University of Manchester</a></li>
+        <li><a href="https://www.physoc.org/abstracts/role-of-the-pulmonary-vein-in-the-genesis-of-atrial-fibrillation-insights-from-a-biophysically-detailed-computational-model/">Abstract in Proceedings of The Physiological Society</a></li>
+        <li><a href="https://github.com/jammastergirish/MPhysAtrialFibrillation">GitHub Repository</a></li>
+    </ul>
+</li>
+</ul>
+</body>
+</html>
